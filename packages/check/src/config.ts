8f63f181aaa48dcfc7d1319454c83296362449bf
Elias Skogevall <elias.skogevall@gmail.com>
2024-04-19 17:18:59 +0200

    Fix interop config module (#11)

--- packages/check/src/config.ts
+++ packages/check/src/config.ts
@@ -3,7 +3,13 @@ import { pathToFileURL } from "node:url";
 
 export interface Config extends CheckOptions {}
 
-export async function loadConfig(path: string): Promise<Config> {
+export async function loadConfig(path: string) {
   const url = pathToFileURL(path).toString();
-  return (await import(url)) as any;
+  return interop(await import(url)) as Config;
+}
+
+function interop(module: object): unknown {
+  return Object.hasOwn(module, "default")
+    ? (module as { default?: unknown }).default
+    : module;
 }
