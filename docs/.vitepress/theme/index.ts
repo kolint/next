69a743f6d5f7b1faed58c3446dfebd381cd81e15
Elias Skogevall <elias.skogevall@gmail.com>
2024-04-20 09:45:42 +0000

    style: format

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,17 @@
 // https://vitepress.dev/guide/extending-default-theme
+import "./custom.css";
 import DefaultTheme, { DefaultTheme as DefaultThemeNs } from "vitepress/theme";
-import './custom.css';
 
 export default DefaultTheme;
 
-export interface ThemeConfig extends Omit<DefaultThemeNs.Config, 'footer'> {
+export interface ThemeConfig extends Omit<DefaultThemeNs.Config, "footer"> {
   footer: {
-    links: FooterLink[]
-    disclaimer: string
-  }
+    links: FooterLink[];
+    disclaimer: string;
+  };
 }
 
 export interface FooterLink {
-  text: string
-  link: string
+  text: string;
+  link: string;
 }
